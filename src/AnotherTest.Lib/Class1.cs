9b7bee00152fb89125e800850ac28f86eb5e0190
Ricardo Bicho <bichori@THEAA.IE>
2021-12-01 23:33:09 +0000

    - Initial commit;

---
+++ src/AnotherTest.Lib/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace AnotherTest.Lib;
+
+public class Class1
+{
+    #region Public Methods
+
+    public string Test()
+    {
+        return "Hello World!";
+    }
+
+    #endregion Public Methods
+}
